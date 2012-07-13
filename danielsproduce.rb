c2b008786e3614cf61681fb04e30cbf2b49fb57c
Aaron Wright <acwrightdesign@gmail.com>
2012-07-13 00:17:16 -0500

    Finished layout.

--- danielsproduce.rb
+++ danielsproduce.rb
@@ -5,14 +5,20 @@ class DanielsProduce < Sinatra::Base
   configure do
     set :root,    File.dirname(__FILE__)
     set :views,   File.join(Sinatra::Application.root, 'views')
+    set :haml,    { :format => :html5, :layout => :'layouts/danielsproduce' }
   end
   
   get '/' do
-    redirect '/index'
+    redirect '/welcome'
   end
   
   get '/:section/?' do
-    erb "#{params[:section]}.html".to_sym
+    @flash = (params[:section] == 'welcome')
+    haml "#{params[:section]}/#{params[:section]}".to_sym
+  end
+  
+  get '/:section/:subsection?' do
+    haml "#{params[:section]}/#{params[:section]}/#{params[:subsection]}/#{params[:subsection]}".to_sym
   end
   
   get '/*' do
